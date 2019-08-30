5b571f3a9df3b569b7869b39d79cc062a6a876e2
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-08-30 14:30:49 +0100

    deletes extra line

--- src/test/scala/OneAdderSpec.scala
+++ src/test/scala/OneAdderSpec.scala
@@ -2,7 +2,6 @@ import collection.mutable.Stack
 import org.scalatest._
 
 class OneAdderSpec extends FlatSpec with Matchers {
-
   "OneAdder" should "add one" in {
     OneAdder.addOne(1) should be (2)
   }
