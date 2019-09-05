d07c8f789f3d372e9ac87e2f35b143efbb879f61
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-09-05 13:57:36 +0100

    adds more Fizzbuzz tests

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -1,3 +1,14 @@
+import scala.math.BigInt
+
 object FizzBuzz {
-  def convert(input:Int) = if (input % 3 == 0) "Fizz"
+  def convert(input:BigInt):String = {
+    if (input != 0) {
+      if (input % 15 == 0) return "FizzBuzz"
+      else if (input % 5 == 0) return "Buzz"
+      else if (input % 3 == 0) return "Fizz"
+      else return input.toString
+    }
+    input.toString
+  }
 }
+
--- src/test/scala/FizzBuzzSpec.scala
+++ src/test/scala/FizzBuzzSpec.scala
@@ -1,8 +1,43 @@
 import collection.mutable.Stack
 import org.scalatest._
+import scala.math.BigInt
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
   "FizzBuzz" should "return Fizz for each multiple of 3" in {
     FizzBuzz.convert(3) should be ("Fizz":String)
   }
+
+  "FizzBuzz" should "return Buzz for each multiple of 5" in {
+    FizzBuzz.convert(5) should be ("Buzz":String)
+  }
+
+  "FizzBuzz" should "return FizzBuzz for each multiple of 3 and 5" in {
+    FizzBuzz.convert(15) should be ("FizzBuzz":String)
+  }
+
+  "FizzBuzz" should "return the input for any other numbers" in {
+    FizzBuzz.convert(2) should be ("2":String)
+  }
+
+  "FizzBuzz" should "return the input for 0" in {
+    FizzBuzz.convert(0) should be("0": String)
+  }
+
+  "FizzBuzz" should "return Fizz for -3" in {
+    FizzBuzz.convert(-3) should be("Fizz": String)
+  }
+
+  "FizzBuzz" should "return the input for -2" in {
+    FizzBuzz.convert(-2) should be("-2": String)
+  }
+
+  "FizzBuzz" should "return FizzBuzz" in {
+    FizzBuzz.convert(3000000) should be("FizzBuzz": String)
+  }
+
+  "FizzBuzz" should "return input" in {
+    FizzBuzz.convert(BigInt(3000000001)) should be("3000000001": String)
+  }
 }
+
+
