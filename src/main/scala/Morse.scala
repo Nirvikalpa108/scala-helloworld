75d6624f30d27d9c7856fdcace0adef8704fc027
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-09-11 12:06:07 +0100

    restructure solution

--- src/main/scala/Morse.scala
+++ src/main/scala/Morse.scala
@@ -3,11 +3,13 @@ import scala.collection.immutable.Map
 object Morse {
   def translate(input:String):String = {
     val dictionary = Map[String, String]("a" -> ".-", "n" -> "-.")
+    // push input into new array
+    // convert each element to morse code
+    // concatenate all elements in the array and add spacing appropriately 
+
     val letters = new Array[String](2)
     var output = ""
     input.split("").foreach(output += dictionary(_))
-    // push output into the empty array
-    // concatenate all elements in the array
     ""
   }
 }
