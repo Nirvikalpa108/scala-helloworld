7ae0c3056a58dd90f558a1191d6bc29b0ffd3f36
Nirvikalpa108 <amina.adewusi@gmail.com>
2019-09-21 08:35:02 +0100

    completes morse solution

--- src/main/scala/Morse.scala
+++ src/main/scala/Morse.scala
@@ -3,11 +3,7 @@ import scala.collection.immutable.Map
 object Morse {
   def translate(input:String):String = {
     val dictionary = Map[String, String]("a" -> ".-", "n" -> "-.")
-    val letters = new Array[String](2)
-    val word = input.split("")
-    for ((letter: String, i: Int) <- word.view.zipWithIndex) {
-      letters.update(i, dictionary(letter))
+    input.split("").map(x => dictionary(x)).mkString(" ")
     }
-    letters.mkString(" ")
   }
-}
+
